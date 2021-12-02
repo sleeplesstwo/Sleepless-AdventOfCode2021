01bd11d139de1df3af2133397b4d032afeaf10c6
SleeplessOne <sleeplesstwo@users.noreply.github.com>
2021-12-01 21:22:15 -0800

    Added comments

--- day02/day02-part01.ps1
+++ day02/day02-part01.ps1
@@ -2,13 +2,19 @@ $BasePath = (Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
 
 $input = Get-Content -Path (Join-Path $BasePath -ChildPath "input.txt")
 
+#Initialize variables for calculations
 $hpos = 0
 $vpos = 0
 
 foreach ($line in $input) {
+    #Split the line into an array on space characters
     $lineArray = $line -split " "
+
+    #Grab each value from the array.
     $direction = $lineArray[0]
     $amount = $lineArray[1]
+
+    #Check direction and perform relevant calculation
     switch ($direction) {
         "up" {
             $vpos = $vpos - $amount
@@ -22,5 +28,6 @@ foreach ($line in $input) {
     }
 }
 
+# Multiply for final results and set the clipboard for pasting the answer.
 $result = $hpos * $vpos
 $result | Set-Clipboard
--- day02/day02-part02.ps1
+++ day02/day02-part02.ps1
@@ -2,14 +2,20 @@ $BasePath = (Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
 
 $input = Get-Content -Path (Join-Path $BasePath -ChildPath "input.txt")
 
+#Initialize variables for calculations
 $hpos = 0
 $vpos = 0
 $aim = 0
 
 foreach ($line in $input) {
+    #Split the line into an array on space characters
     $lineArray = $line -split " "
+
+    #Grab each value from the array.
     $direction = $lineArray[0]
     $amount = $lineArray[1]
+    
+    #Check direction and perform relevant calculation
     switch ($direction) {
         "up" {
             $aim = $aim - $amount
@@ -24,5 +30,6 @@ foreach ($line in $input) {
     }
 }
 
+# Multiply for final results and set the clipboard for pasting the answer.
 $result = $hpos * $vpos
 $result | Set-Clipboard
