ec9343e50aae8fecfb676717a88d681ed3836a3d
SleeplessOne <sleeplesstwo@users.noreply.github.com>
2021-12-02 00:00:16 -0800

    Add solution as a function.

---
+++ day02/day02-AsFunction.ps1
@@ -0,0 +1,62 @@
+function Get-Day2Solution {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $InputFile,
+
+        # Parameter help description
+        [Parameter()]
+        [ValidateRange(1,2)]
+        [int]
+        $Part = 1
+    )
+
+    begin {
+        #Import input file with space character as delimiter and initialize variables used for calculation
+        $values = Import-CSV -Path $InputFile -Header "direction","amount" -Delimiter " "
+        $hpos = 0
+        $vpos = 0
+        $aim = 0
+    }
+
+    process {
+        foreach ($line in $values) {
+            if ($Part -eq 1) {
+                switch ($line.direction) {
+                    "up" {
+                        $vpos = $vpos - $line.amount
+                    }
+                    "down" {
+                        $vpos = $vpos + $line.amount
+                    }
+                    "forward" {
+                        $hpos = $hpos + $line.amount
+                    }
+                }
+            }
+            elseif ($Part -eq 2) {
+                switch ($line.direction) {
+                    "up" {
+                        $aim = $aim - $line.amount
+                    }
+                    "down" {
+                        $aim = $aim + $line.amount
+                    }
+                    "forward" {
+                        $hpos = $hpos + $line.amount
+                        $vpos = $vpos + ($aim * $line.amount)
+                    }
+                }
+            }
+        }  
+    }
+
+    end {
+        $result = $hpos * $vpos
+        return $result
+    }
+}
+
+Get-Day2Solution -InputFile ".\input.txt" -Part 1
+Get-Day2Solution -InputFile ".\input.txt" -Part 2
