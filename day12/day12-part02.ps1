547910b0d6f6f6a49c2c1abdb1e15e9d3d6f5792
SleeplessOne <sleeplesstwo@users.noreply.github.com>
2021-12-12 15:07:13 -0800

    Added comments to part 2

--- day12/day12-part02.ps1
+++ day12/day12-part02.ps1
@@ -2,6 +2,7 @@ $BasePath = (Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
 
 $CaveData = (Get-Content -Path (Join-Path $BasePath -ChildPath "input.txt"))
 
+# Load the cave connections into an object to track the links.
 [System.Collections.ArrayList]$Global:AllLinks = @()
 foreach ($line in $CaveData) {
     $linelinks = $line -split "-"
@@ -12,6 +13,7 @@ foreach ($line in $CaveData) {
     $Global:AllLinks.Add($LinkData) | Out-Null
 }
 
+# Start with an empty arraylist of paths taken to reach the end.
 [System.Collections.ArrayList]$Global:PathsTaken = @()
 
 function Move-Room {
@@ -25,24 +27,33 @@ function Move-Room {
         [string[]]
         $InvalidRooms
     )
+
+    # Get a list of the links where either A or B side is equal to the last item in the current path (current room) and is not listed in invalid rooms
     $PotentialMoves = $Global:AllLinks | Where-Object { ($_.SideA -eq $CurrentPath[-1] -or $_.SideB -eq $CurrentPath[-1]) -and $_.SideA -notin $InvalidRooms -and $_.SideB -notin $InvalidRooms }
 
+    # Update the invalid rooms with the current path
     $InvalidRooms = Test-ValidRoom -CurrentPath $CurrentPath
 
     foreach ($move in $PotentialMoves) {
+        # If the current room is on SideA we move to SideB
         if ($move.SideA -eq $CurrentPath[-1]) {
             if ($move.SideB -eq "end") {
+                # If the move it to end then we've completed a path, add the path to our arraylist of paths taken
                 $Global:PathsTaken.Add($CurrentPath + "end") | Out-Null
             }
             else {
+                # Otherwise move to the new room by recursively calling the function
                 Move-Room -CurrentPath ($CurrentPath + $move.SideB) -InvalidRooms $InvalidRooms
             }
         }
+        # Otherwise if we're on SideB of the link we move to SideA
         elseif ($move.SideB -eq $CurrentPath[-1]) {
             if ($move.SideA -eq "end") {
+                # If the move it to end then we've completed a path, add the path to our arraylist of paths taken
                 $Global:PathsTaken.Add($CurrentPath + "end") | Out-Null
             }
             else {
+                # Otherwise move to the new room by recursively calling the function
                 Move-Room -CurrentPath ($CurrentPath + $move.SideA) -InvalidRooms $InvalidRooms
             }
         }
@@ -56,12 +67,15 @@ function Test-ValidRoom {
         $CurrentPath
     )
     [System.Collections.ArrayList]$InvalidRooms = @()
+    # Find all the small rooms in our current path
     $SmallRooms = $CurrentPath | Where-Object { ($_ -cmatch "^[a-z]*$") -and $_ -ne "start"}
 
+    # Have we been in any small rooms twice?  If not then all small rooms are still valid
     $DoubledRoom = $SmallRooms | Group-Object | Where-Object {$_.Count -ge 2}
     if ($DoubledRoom.length -eq 0) {
         $InvalidRooms.Add("start") | Out-Null
     } else {
+        # Otherwise all small rooms are now invalid
         $InvalidRooms.Add("start") | Out-Null
         foreach ($room in (($SmallRooms | Group-Object).Name)) {
             $InvalidRooms.Add($room) | Out-Null
